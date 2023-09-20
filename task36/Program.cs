8e7a719a2e30c02735ede9d8dd975cd8e1232ea1
Juliya_K <moyangelu1987@gmail.com>
2023-09-20 18:03:35 +0300

    add Task 34 36 38

---
+++ task36/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+Console.Write("Введите количество элементов массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] number = new int[size];
+FillArrayRandomNumber(number);
+PrintArray(number);
+
+int sumNumberIndex = 0;
+
+for (int i = 0; i < number.Length; i += 2)
+{
+    sumNumberIndex += number[i];
+}
+Console.Write(sumNumberIndex);
+
+
+
+void FillArrayRandomNumber(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 101);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ task38/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76*/
+
+double[] doubles = { 3.22, 4.2, 1.15, 77.15, 65.2 };
+
+double differenceBetweenMaxAndMin(double[] doubles)
+{
+    double min = doubles[0];
+    double max = 0;
+
+    for (int i = 0; i < doubles.Length; i++)
+    {
+        if (doubles[i] > max)
+            max = doubles[i];
+
+        if (doubles[i] < min)
+            min = doubles[i];
+    }
+
+    return max - min;
+}
+
+Console.WriteLine(differenceBetweenMaxAndMin(doubles));
